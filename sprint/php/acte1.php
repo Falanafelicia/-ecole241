ef3b6d107b08bf2cb57db9bdf77aece6b53ae7ab
Falanafelicia <44722518+Falanafelicia@users.noreply.github.com>
2019-03-26 13:15:22 +0000

    modification du php

--- sprint/php/acte1.php
+++ sprint/php/acte1.php
@@ -2,11 +2,11 @@
 <html>
 <body>
  <h1>Mon premier PHP</h1>
- 
-	<?php
-	$color = "OKACODE";
-	echo "Ma famille" . $color .;
-	?>
+
+<?php
+$color = "OKACODE";
+echo "Ma famille $color";
+?>
 
 </body>
 </html>
