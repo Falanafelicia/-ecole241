c2461af259589be75e431ed64f7117362d344d4f
Falanafelicia <44722518+Falanafelicia@users.noreply.github.com>
2019-03-26 16:52:57 +0000

    modif acte2

--- sprint/php/acte2.php
+++ sprint/php/acte2.php
@@ -1,24 +1,23 @@
 <!DOCTYPE html>
-<html>
-<head>
-		<title>VARIABLES</title>
-</head>
-<body>
-	<h1>Mes variables PHP</h1>
-	<h5>J'ai donc pu trouver 4 types de variables en php</h3>
+ <html>
+ <head>
+         <title>VARIABLES</title>
+ </head>
+ <body>
+     <h1>Mes variables PHP</h1>
+     <h5>J'ai donc pu trouver 4 types de variables en php</h5>
 
- <?php
+  <?php
 
- $nom = "FALANA";    // Type string (chaine de caractères)
- $age = 20;    // Type entier(nombres entiers)
- $des = 5e-1;    // Type réels/floats
- $un = true;  // Type booléen
- $cours = array("leadership","management","informatique"); // Type tableau
+  $nom = "FALANA";    // Type string (chaine de caractères)
+  $age = 20;    // Type entier(nombres entiers)
+  $des = 5e-1;    // Type réels/floats
+  $un = true;  // Type booléen
+  $cours = array("leadership","management","informatique"); // Type tableau
 
- echo "Moi je suis $nom et je bois $des litres d'eau par jour,j'ai $age ans et ceci est donc $un aperçu<br>"; 
- //echo "Je fais" .$cours[0]. "," .$cours[1]. "et" .$cours[2].;
+  echo "Moi je suis $nom et je bois $des litres d'eau par jour,j'ai $age ans et ceci est donc $un aperçu<br>";
+  echo "Je fais {$cours[0]} , {$cours[1]} et {$cours[2]}";
+  ?>
 
- ?>
-
-</body>
-</html>
+ </body>
+ </html>
