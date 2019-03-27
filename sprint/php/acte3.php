bdb09fdd62228fffd7c281ed704f4cd994af37a6
Falanafelicia <44722518+Falanafelicia@users.noreply.github.com>
2019-03-27 09:26:53 +0000

    ajout du dossier acte3

---
+++ sprint/php/acte3.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Variables-Tableaux-Array</title>
+</head>
+<body>
+    <?php
+    $prénoms = ("Falana","Hamilton","Boris","Jean","Didier","Grâce","Pauline","Line","Din","Yvan");
+    echo ="Je fais {$prénoms[0]} , {$prénoms[1]} , $prénoms[2]} , $prénoms[3]}, $prénoms[4]}, $prénoms[5]}, $prénoms[6]}, $prénoms[7]}, $prénoms[8]}, $prénoms[9]}";
+    ?>
+
+</body>
+</html>
