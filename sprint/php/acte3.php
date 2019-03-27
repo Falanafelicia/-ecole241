eb6f26a0da208225422edf7bfb59faa585489f3e
Falanafelicia <44722518+Falanafelicia@users.noreply.github.com>
2019-03-27 09:59:22 +0000

    rectification du dossier acte3

--- sprint/php/acte3.php
+++ sprint/php/acte3.php
@@ -5,8 +5,8 @@
 </head>
 <body>
     <?php
-    $prénoms = ("Falana","Hamilton","Boris","Jean","Didier","Grâce","Pauline","Line","Din","Yvan");
-    echo ="Je fais {$prénoms[0]} , {$prénoms[1]} , $prénoms[2]} , $prénoms[3]}, $prénoms[4]}, $prénoms[5]}, $prénoms[6]}, $prénoms[7]}, $prénoms[8]}, $prénoms[9]}";
+    $prenoms = array ("Falana","Hamilton","Boris","Jean","Didier","Grâce","Pauline","Line","Din","Yvan");
+    echo "Je connais {$prenoms[0]} , {$prenoms[1]} , {$prenoms[2]} , {$prenoms[3]}, {$prenoms[4]}, {$prenoms[5]}, {$prenoms[6]}, {$prenoms[7]}, {$prenoms[8]}, {$prenoms[9]}";
     ?>
 
 </body>
