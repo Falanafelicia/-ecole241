9485e56e4c3ab28b54dc6bbb5b5da4b024590fc9
Falanafelicia <44722518+Falanafelicia@users.noreply.github.com>
2019-03-27 10:02:32 +0000

    modifications acte3

--- sprint/php/acte3.php
+++ sprint/php/acte3.php
@@ -6,7 +6,7 @@
 <body>
     <?php
     $prenoms = array ("Falana","Hamilton","Boris","Jean","Didier","Grâce","Pauline","Line","Din","Yvan");
-    echo "Je connais {$prenoms[0]} , {$prenoms[1]} , {$prenoms[2]} , {$prenoms[3]}, {$prenoms[4]}, {$prenoms[5]}, {$prenoms[6]}, {$prenoms[7]}, {$prenoms[8]}, {$prenoms[9]}";
+    echo "Je connais {$prenoms[0]} , {$prenoms[1]} , {$prenoms[2]} , {$prenoms[3]} , {$prenoms[4]} , {$prenoms[5]} , {$prenoms[6]} , {$prenoms[7]} , {$prenoms[8]} , {$prenoms[9]}";
     ?>
 
 </body>
